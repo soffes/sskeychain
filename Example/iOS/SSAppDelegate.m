9824364b82fdcdb801d986bc0cc26b095e6fd437
liamnichols <liam.nichols.ln@gmail.com>
2014-09-01 19:29:01 +0100

    removed test code

--- Example/iOS/SSAppDelegate.m
+++ Example/iOS/SSAppDelegate.m
@@ -11,35 +11,7 @@
 
 @implementation SSAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	
-//	SSKeychainQuery *query = [[SSKeychainQuery alloc] init];
-//	query.service = @"serviceName";
-//	query.account = @"accountName";
-//	query.password = @"securedPassword";
-//	query.useNoAuthenticationUI = @YES;
-//	query.accessControl = [SSKeychainAccessControl accessControlWithAccessibility:SSKeychainAccessibilityWhenUnlockedThisDeviceOnly flags:SSKeychainCreateFlagUserPresence];
-//	
-//	
-//	NSError *error = nil;
-//	BOOL result = [query save:&error];
-
-	
-	
-	
-	SSKeychainQuery *query = [[SSKeychainQuery alloc] init];
-	query.service = @"serviceName";
-	query.account = @"accountName";
-
-	
-	dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-		NSError *error = nil;
-		BOOL result = [query fetch:&error];
-		NSLog(@"Result: %@", result ? @"Success" : @"Failure");
-		NSLog(@"Error: %@", error);
-		NSLog(@"Data: %@", query.passwordData);
-	});
-	
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {	
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
