a7caf8144117d2652d7678cebc75e2b32c33eef3
Roger Oba <rogerluan.oba@gmail.com>
2018-04-17 15:13:03 -0300

    Remove 2 unnecessary keychain creation flags, simplifying interfaces.

--- Sources/SAMKeychainAccessControl.h
+++ Sources/SAMKeychainAccessControl.h
@@ -39,42 +39,41 @@ typedef NS_OPTIONS(NSUInteger, SAMKeychainCreateFlags) {
 	 User presence policy using biometry or Passcode. Biometry does not have to be available or enrolled. Item is still
 	 accessible by Touch ID even if fingers are added or removed. Item is still accessible by Face ID if user is re-enrolled.
 	 */
-	SAMKeychainCreateFlagUserPresence 			= 1UL << 0,
+	SAMKeychainCreateFlagUserPresence 			= 1u << 0,
+
 	/** kSecAccessControlBiometryAny
 	 Constraint: Touch ID (any finger) or Face ID. Touch ID or Face ID must be available. With Touch ID
 	 at least one finger must be enrolled. With Face ID user has to be enrolled. Item is still accessible by Touch ID even
 	 if fingers are added or removed. Item is still accessible by Face ID if user is re-enrolled.
 	 */
-	SAMKeychainCreateFlagBiometryAny			CF_ENUM_AVAILABLE(10_13_4, 11_3) = 1u << 1,
-	/** kSecAccessControlTouchIDAny
-	 Deprecated, please use kSecAccessControlBiometryAny instead.
-	 */
-	SAMKeychainCreateFlagTouchIDAny             API_DEPRECATED_WITH_REPLACEMENT("kSecAccessControlBiometryAny", macos(10.12.1, 10.13.4), ios(9.0, 11.3)) = 1u << 1,
+	SAMKeychainCreateFlagBiometryAny			CF_ENUM_AVAILABLE(10_12_1, 9_0) = 1u << 1,
+
 	/** kSecAccessControlBiometryCurrentSet
 	 Constraint: Touch ID from the set of currently enrolled fingers. Touch ID must be available and at least one finger must
 	 be enrolled. When fingers are added or removed, the item is invalidated. When Face ID is re-enrolled this item is invalidated.
 	 */
-	SAMKeychainCreateFlagBiometryCurrentSet     CF_ENUM_AVAILABLE(10_13_4, 11_3) = 1u << 3,
-	/** kSecAccessControlTouchIDCurrentSet
-	 Deprecated, please use kSecAccessControlBiometryCurrentSet instead.
-	 */
-	SAMKeychainCreateFlagTouchIDCurrentSet      API_DEPRECATED_WITH_REPLACEMENT("kSecAccessControlBiometryCurrentSet", macos(10.12.1, 10.13.4), ios(9.0, 11.3)) = 1u << 3,
+	SAMKeychainCreateFlagBiometryCurrentSet     CF_ENUM_AVAILABLE(10_12_1, 9_0) = 1u << 3,
+
 	/** kSecAccessControlDevicePasscode
 	 Constraint: Device passcode
 	 */
 	SAMKeychainCreateFlagDevicePasscode         CF_ENUM_AVAILABLE(10_11, 9_0) = 1u << 4,
+
 	/** kSecAccessControlOr
 	 Constraint logic operation: when using more than one constraint, at least one of them must be satisfied.
 	 */
 	SAMKeychainCreateFlagOr                     CF_ENUM_AVAILABLE(10_12_1, 9_0) = 1u << 14,
+
 	/** kSecAccessControlAnd
 	 Constraint logic operation: when using more than one constraint, all must be satisfied.
 	 */
 	SAMKeychainCreateFlagAnd                    CF_ENUM_AVAILABLE(10_12_1, 9_0) = 1u << 15,
+
 	/** kSecAccessControlPrivateKeyUsage
 	 Create access control for private key operations (i.e. sign operation)
 	 */
 	SAMKeychainCreateFlagPrivateKeyUsage        CF_ENUM_AVAILABLE(10_12_1, 9_0) = 1u << 30,
+
 	/** kSecAccessControlApplicationPassword
 	 Security: Application provided password for data encryption key generation. This is not a constraint but additional item
 	 encryption mechanism.
